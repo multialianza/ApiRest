11366f3a77a0904a0250ac4e23f45cae8dd2e13e
ivan <ivansalazarzepeda@gmail.com>
2024-07-22 01:28:55 -0400

    compaginando modificaciones

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <div v-if="loading">Cargando...</div>
+    <div v-else class="container">
+      <div class="user">
+        <img :src="usuario1.picture.large" :alt="usuario1.name.first" />
+        <p>{{ usuario1.name.first }} {{ usuario1.name.last }}</p>
+        <input v-model="nuevoMensaje1" placeholder="Escribe un mensaje..." />
+        <button @click="agregarMensaje(usuario1, nuevoMensaje1, '#007bff')">
+          Enviar
+        </button>
+      </div>
+      <ChatComponent :mensajes="mensajes" />
+      <div class="user">
+        <img :src="usuario2.picture.large" :alt="usuario2.name.first" />
+        <p>{{ usuario2.name.first }} {{ usuario2.name.last }}</p>
+        <input v-model="nuevoMensaje2" placeholder="Escribe un mensaje..." />
+        <button @click="agregarMensaje(usuario2, nuevoMensaje2, '#dc3545')">
+          Enviar
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import ChatComponent from './components/ChatComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ChatComponent,
+  },
+  data() {
+    return {
+      usuario1: null,
+      usuario2: null,
+      nuevoMensaje1: '',
+      nuevoMensaje2: '',
+      mensajes: [],
+      loading: true,
+    };
+  },
+  mounted() {
+    this.obtenerUsuarios();
+  },
+  methods: {
+    obtenerUsuarios() {
+      axios
+        .get('https://randomuser.me/api/?results=2')
+        .then((response) => {
+          this.usuario1 = response.data.results[0];
+          this.usuario2 = response.data.results[1];
+          this.loading = false;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    agregarMensaje(usuario, mensaje, color) {
+      if (mensaje.trim() !== '') {
+        this.mensajes.push({
+          usuario: `${usuario.name.first} ${usuario.name.last}`,
+          mensaje,
+          color,
+        });
+        this.nuevoMensaje1 = '';
+        this.nuevoMensaje2 = '';
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/* Estilos CSS */
 </style>
---
+++ src/components/ChatComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="chat">
+    <div
+      v-for="(mensaje, index) in mensajes"
+      :key="index"
+      :style="{ backgroundColor: mensaje.color }"
+      class="mensaje"
+    >
+      <p>{{ mensaje.usuario }}</p>
+      <p>{{ mensaje.mensaje }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatComponent',
+  props: {
+    mensajes: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chat {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 400px;
+  height: 500px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  overflow-y: auto;
+}
+
+.mensaje {
+  padding: 10px;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  max-width: 80%;
+  word-wrap: break-word;
+}
+</style>
